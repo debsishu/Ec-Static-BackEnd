8f9e3bfe3ce917a2f4bca76b379b9099670878a6
Debsishu Pal <pauldebsishu2002@gmail.com>
2022-10-18 22:21:17 +0530

    Cors Fix

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 6969;
-require("dotenv").config();
 
-const routes = require("./routes");
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.status(200).json("API working fine");
 });
 
-app.use(cors());
+var whitelist = JSON.parse(process.env.LIST_VAR);
+var corsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
+
+const routes = require("./routes");
 app.use("/api", routes);
 
 app.listen(PORT, () => {
