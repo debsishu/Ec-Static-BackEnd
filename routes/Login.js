81ccc5bc428626ec25eed1632a7c7f1e56c3816e
Debsishu Pal <pauldebsishu2002@gmail.com>
2022-10-29 23:46:43 +0530

    Login Route Error Handling Add

--- routes/Login.js
+++ routes/Login.js
@@ -6,32 +6,34 @@ const User = require("../models/User");
 const JWTSECRET = process.env.JWTSECRET;
 
 route.post("/", async (req, res) => {
-  const { username, password } = req.body;
-
-  const user = await User.findOne({ username }).lean();
-  if (!user) {
-    res.sendStatus(401).json({
-      status: "invalid-username-or-password",
-      message: "username or password is invalid",
-    });
-  }
-  if (await bcrypt.compare(password, user.password)) {
-    const token = jwt.sign(
-      { username: user.username, id: user._id },
-      JWTSECRET
-    );
-    return res.json({
-      id: user._id,
-      name: user.name,
-      username: user.username,
-      profileImageURL: user.profileImageURL,
-      token: token,
-    });
-  } else {
-    res.sendStatus(401).json({
-      status: "invalid-username-or-password",
-      message: "username or password is invalid",
-    });
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username }).lean();
+    if (!user) {
+      res.status(401).json({
+        status: "invalid-username-or-password",
+        message: "username or password is invalid",
+      });
+    } else if (await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign(
+        { username: user.username, id: user._id },
+        JWTSECRET
+      );
+      return res.json({
+        id: user._id,
+        name: user.name,
+        username: user.username,
+        profileImageURL: user.profileImageURL,
+        token: token,
+      });
+    } else {
+      res.status(401).json({
+        status: "invalid-username-or-password",
+        message: "username or password is invalid",
+      });
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
