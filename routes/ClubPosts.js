eafdab754debc332fcc416152abc116bc3dc8d0d
Debsishu Pal <debsishu@amazon.com>
2023-04-21 00:14:55 +0530

    ClubPosts bug fix

--- routes/ClubPosts.js
+++ routes/ClubPosts.js
@@ -2,20 +2,23 @@ const express = require("express");
 const route = express.Router();
 const verifyToken = require("../middleware/VerifyToken");
 const Posts = require("../models/Posts");
+const Clubs = require("../models/Clubs");
 
-route.post("/clubposts", verifyToken, async(req,res)=>{
-    const clubId = req.params.clubID;
-
-    try{
-        const clubPosts = await Posts.find({"clubID" : clubId});
-        console.log("Fetching club posts");
-        res.status(200).json({ post: clubPosts });
-    }catch(err){
-        console.log(err);
-        res.status(404).json({message : "club-not-found"})
+route.get("/", verifyToken, async (req, res) => {
+  const clubId = req.query.clubID;
+  try {
+    const isClubAvailable = await Clubs.find({ Cname: clubId });
+    if (!isClubAvailable || isClubAvailable.length == 0) {
+      res.status(404).json({ message: "club-not-found" });
+    } else {
+      const clubPosts = await Posts.find({ clubID: clubId });
+      console.log(`Fetching ${clubId} club posts`);
+      res.status(200).json({ posts: clubPosts });
     }
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({ err });
+  }
 });
 
 module.exports = route;
-
-
